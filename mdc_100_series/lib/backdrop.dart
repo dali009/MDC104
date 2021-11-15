4aa4d62498a35fc4d79ea27017d6f123bf5b4aa1
dali009 <dalifatnassi20@gmail.com>
2021-11-15 01:16:45 +0100

    adding motion

--- mdc_100_series/lib/backdrop.dart
+++ mdc_100_series/lib/backdrop.dart
@@ -10,6 +10,7 @@ import 'model/product.dart'; // New code
 import 'supplemental/cut_corners_border.dart';
 
 // TODO: Add velocity constant (104)
+const double _kFlingVelocity = 2.0;
 
 class Backdrop extends StatefulWidget {
   final Category currentCategory;
@@ -39,17 +40,81 @@ class _BackdropState extends State<Backdrop>
   final GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
 
   // TODO: Add AnimationController widget (104)
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      value: 1.0,
+      vsync: this,
+    );
+  }
+
+  // TODO: Add override for didUpdateWidget (104)
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // TODO: Add functions to get and change front layer visibility (104)
+  bool get _frontLayerVisible {
+    final AnimationStatus status = _controller.status;
+    return status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+  }
+
+  void _toggleBackdropLayerVisibility() {
+    _controller.fling(
+        velocity: _frontLayerVisible ? -_kFlingVelocity : _kFlingVelocity);
+  }
 
   // TODO: Add BuildContext and BoxConstraints parameters to _buildStack (104)
-  Widget _buildStack() {
+  Widget _buildStack(BuildContext context, BoxConstraints constraints) {
+    const double layerTitleHeight = 48.0;
+    final Size layerSize = constraints.biggest;
+    final double layerTop = layerSize.height - layerTitleHeight;
+
+    // TODO: Create a RelativeRectTween Animation (104)
+    Animation<RelativeRect> layerAnimation = RelativeRectTween(
+      begin: RelativeRect.fromLTRB(
+          0.0, layerTop, 0.0, layerTop - layerSize.height),
+      end: const RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
+    ).animate(_controller.view);
+
     return Stack(
       key: _backdropKey,
       children: <Widget>[
         // TODO: Wrap backLayer in an ExcludeSemantics widget (104)
-        widget.backLayer,
-        widget.frontLayer,
+        ExcludeSemantics(
+          child: widget.backLayer,
+          excluding: _frontLayerVisible,
+        ),
+        // TODO: Add a PositionedTransition (104)
+        PositionedTransition(
+          rect: layerAnimation,
+          child: _FrontLayer(
+            // TODO: Implement onTap property on _BackdropState (104)
+            child: widget.frontLayer,
+          ),
+        ),
       ],
     );
+  }
+  Widget _buildStack() {
+    return Stack(
+      key: _backdropKey,
+      children: <Widget>[
+    // TODO: Wrap backLayer in an ExcludeSemantics widget (104)
+    ExcludeSemantics(
+    child: widget.backLayer,
+      excluding: _frontLayerVisible,
+    ),
+    ...
+
   }
 
   @override
@@ -89,7 +154,7 @@ class _BackdropState extends State<Backdrop>
     return Scaffold(
       appBar: appBar,
       // TODO: Return a LayoutBuilder widget (104)
-      body: _buildStack(),
+      body: LayoutBuilder(builder: _buildStack),
     );
   }
 }
